0547a4cf1fe5272f3680ad9ae5d9742b0a0af7db
Wolff09 <s_wolff09@cs.uni-kl.de>
2013-06-12 20:15:47 +0200

    __main__ gerüst

--- __main__.py
+++ __main__.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import sys
+from handler import handle_scanning as scan
+
+def main(*cmd_args):
+	scan()
+
+if __name__ == '__main__':
+	main(*sys.argv[1:])
