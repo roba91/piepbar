351b0c0729b5cfbc65a86da774f6d674c34a6573
Frederik Walk <f_walk09@cs.uni-kl.de>
2013-06-13 14:49:06 +0200

    Display methods

--- display/display.py
+++ display/display.py
@@ -7,13 +7,15 @@ import Image, ImageDraw, ImageFont
 
 base = None
 screen = None
-font = ImageFont.truetype("LCD.ttf", 12) #TODO move to init
+font = None
+font_large = None
 
 SCREEN_PATH = path.join(path.dirname(path.realpath(__file__)), 'screen_base.png')
+FONT_PATH = path.join(path.dirname(path.realpath(__file__)), 'Terminus.ttf')
+BOLDFONT_PATH = path.join(path.dirname(path.realpath(__file__)), 'TerminusBold.ttf')
 
 def display_init():
-<<<<<<< HEAD:display.py
-	global screen, base
+	global screen, base, font, font_large
 
 	try:
 		base = Image.open(SCREEN_PATH)
@@ -28,19 +30,31 @@ def display_init():
 	base = base.convert("1")
 	screen = base.copy()
 
-def display_mainview():
-	global screen
-	screen = base.copy()
-	draw = ImageDraw.Draw(screen)
-	draw.text((10, 25), "OH HAI!", font=font)
+	try:
+		font = ImageFont.truetype(FONT_PATH, 14)
+		font_large = ImageFont.truetype(BOLDFONT_PATH, 14)
+	except:
+		print("Error: loading font!")
+		sys.exit()
 
-	display_draw()
+def display_empty():
+	display_name("")
+	display_products("","")
 
 def display_name(name):
-	pass
+	global screen
+	draw = ImageDraw.Draw(screen)
+	draw.rectangle(((0,0),(159,14)), fill=255)
+	draw.line([(0,14),(159,14)], fill=0)
+	draw.line([(0,16),(159,16)], fill=0)
+	draw.text((1,1), name, font=font_large)
 
-def diplay_add_drink(drink, price):
-	pass
+def display_products(drinks,sum):
+	global screen
+	draw = ImageDraw.Draw(screen)
+	draw.rectangle(((0,17),(159,79)), fill=255)
+	draw.line([(0,65),(159,65)], fill=0)
+	draw.text([a-b for a,b in zip((158,82),font.getsize("Summe: %s" % sum))], "Summe: %s" % sum, font=font)
 
 def display_idle():
 	display_mainview()
